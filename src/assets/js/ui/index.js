c0ca2ae215bf659b00d2f57420bdd201caca3793
aisteron <timotheus@list.ru>
2024-06-19 23:18:33 +0300

    w19.06

--- src/assets/js/ui/index.js
+++ src/assets/js/ui/index.js
@@ -1,5 +1,45 @@
+import { qs } from "../libs";
 import { swipers } from "./swipers";
 
-export function Ui(){
+export async function Ui(){
+
 	swipers()
+
+	await masonry.load()
+	masonry.init()
+	
+}
+
+const masonry = {
+
+	async load(){
+
+	return new Promise(resolve => {
+
+		let grid_wrapper = qs('.blog-page .grid')
+		if(!grid_wrapper) return
+		
+		if(qs('[msr]')) resolve()
+
+		let script = document.createElement('script')
+		script.src = "/vendors/masonry.pkgd.min.js"
+		script.setAttribute('msr','')
+
+		qs('.scripts-area').appendChild(script)
+		script.onload = () => resolve()
+
+	})
+	
+	},
+	init(){
+		let grid_wrapper = qs('.blog-page .grid')
+		if(!grid_wrapper) return
+		if(window.innerWidth <= 480){console.log('do not init masonry');return}
+		
+		var msnry = new Masonry( grid_wrapper, {
+			itemSelector: '.grid-item',
+			columnWidth: 273,
+			gutter: 16
+		});
+	}
 }
