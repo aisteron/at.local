915f8400bb81f5e287f5323c45fdd72af2f31c49
aisteron <timotheus@list.ru>
2024-09-09 15:04:08 +0300

    w09.09

--- src/assets/js/ui/index.js
+++ src/assets/js/ui/index.js
@@ -114,24 +114,43 @@ async function filter_tours(){
 		
 		
 		g.forEach(el => {
-			let start = new Date(el.date.start)
-			let finish = new Date(el.date.finish)
-			let date = ``
-			let monf = finish.getMonth() < 10 ? '0'+ (finish.getMonth()+1) : finish.getMonth()+1
-			let mons = start.getMonth() < 10 ? '0'+ (start.getMonth()+1) : start.getMonth()+1
 
-			if(start.getMonth() == finish.getMonth()){
-				date = `${start.getDate()} - ${finish.getDate()}.${monf}.${finish.getFullYear()}`
-			} else {
-				date = `${start.getDate()}.${mons} - ${finish.getDate()}.${finish.getMonth()}`
-			}
+
 			
+			
+			let label, price,start,finish,date,monf,mons, least;
+
+			if(el.date.length == 0){ // актуальных дат нет
+				label = `<div class="label red"><span>Мест нет</span></div>`
+				price = '___ ___'
+				date = '___'
+				least = '___'
+			} else {
+				label = `<div class="label yellow"><span>${el?.date?.seats} ${declension("место", "мест","места", el?.date?.seats)}</span></div>`
+				price = el.date.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ") + ' '+el.currency
+
+				start = new Date(el?.date.start)
+				finish = new Date(el?.date.finish)
+				date = ``
+				monf = finish.getMonth() < 10 ? '0'+ (finish.getMonth()+1) : finish.getMonth()+1
+				mons = start.getMonth() < 10 ? '0'+ (start.getMonth()+1) : start.getMonth()+1
+
+				if(start.getMonth() == finish.getMonth()){
+					date = `${start.getDate()} - ${finish.getDate()}.${monf}.${finish.getFullYear()}`
+				} else {
+					date = `${start.getDate()}.${mons} - ${finish.getDate()}.${finish.getMonth()}`
+				}
+
+				least = `+ ${el.least} ${declension("дата", "дат","даты", el.least)}`
+			}
+
+
+
 			str += `
 			<li>
 
-				<div class="label yellow">
-					<span>${el.date.seats} ${declension("место", "мест","места", el.date.seats)}</span>
-				</div>
+				${label}
+				
 
 				<img class="hero" src="${host}${el.thumb}" width="320" height="214">
 				
@@ -139,12 +158,12 @@ async function filter_tours(){
 					<span class="country">${el.country}</span>
 					<a href="${el.uri}">${el.pagetitle}</a>
 					<div class="price">
-						<span class="green">${el.date.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ")} ${el.currency}</span>
+						<span class="green">${price}</span>
 						<span class="duration">${el.duration} ${declension("день", "дней","дня", el.duration)}</span>
 					</div>
 					<div class="dates">
 						<div class="actual">${date}</div>
-						<div class="plan">+ ${el.least} ${declension("дата", "дат","даты", el.least)}</div>
+						<div class="plan">${least}</div>
 					</div>
 				</div>
 
