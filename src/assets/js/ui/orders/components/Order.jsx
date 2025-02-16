8ca93eaf375fff1b513ade1bb123f8104298dd68
aisteron <timotheus@list.ru>
2025-02-17 00:53:39 +0300

    w17.12

--- src/assets/js/ui/orders/components/Order.jsx
+++ src/assets/js/ui/orders/components/Order.jsx
@@ -13,10 +13,18 @@ export const Order = ({ el }) => {
 	}
 
 	const approve = async el => {
-		setApproved(1);
+
 		let res = await Fetch("approve_order", { id: el.id, clientid: el.clientid }, "/assets/php/orders/")
 		let message = res?.success ? '✅ Approved' : '❌ Ошибка обновления'
 		new Snackbar(message)
+		res?.success && setApproved(1);
+	}
+
+	let d = {
+		created_at_date: new Date(el.createdat).toLocaleDateString(),
+		created_at_time: new Date(el.createdat).toLocaleTimeString(),
+		tour_date: new Date(el.tour_date).toLocaleDateString()
+
 	}
 
 	return (
@@ -24,9 +32,9 @@ export const Order = ({ el }) => {
 			<div className="num">
 				<input type="text" value={num} onChange={e => update_num(e)} />
 			</div>
-			<span className="createdat">{el.createdat}</span>
+			<span className="createdat">{d.created_at_date}<br />{d.created_at_time}</span>
 			<span className="tour_name">{el.tour_name}</span>
-			<span className="tour_date">{el.tour_date}</span>
+			<span className="tour_date">{d.tour_date}</span>
 			<span className="tourist_name">{el.tourist_name}</span>
 			<span className="tourist_email">{el.tourist_email}</span>
 			<span className="tourist_phone">{el.tourist_phone}</span>
